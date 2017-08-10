077de77ccdeff90c18844215204d83cd91050938
jennifersd <jennifersduenas@gmail.com>
2017-08-10 13:19:28 +0200

    changes

--- wp-config.php
+++ wp-config.php
@@ -14,6 +14,13 @@
  * @package WordPress
  */
 
+define('FORCE_SSL_ADMIN', true);
+// in some setups HTTP_X_FORWARDED_PROTO might contain 
+// a comma-separated list e.g. http,https
+// so check for https existence
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
+       $_SERVER['HTTPS']='on';
+
 // ** Heroku Postgres settings - from Heroku Environment ** //
 $db = parse_url($_ENV["DATABASE_URL"]);
 
